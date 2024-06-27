0387937c0707c70112a2b5da649a2db1c3ac562a
DAMIENS Lana <lana.damiens@telecomnancy.eu>
2024-06-27 10:20:21 +0100

    initial commit +v1

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+
+import Header from './components/Header.jsx';
+import UserInput from './components/UserInput.jsx';
+import Results from './components/Results.jsx';
+
+function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue, //On ajoute un plus car sinon en js les additions donnent des concatenation de chaines de caracteres et non pas des additions
+        //On ajoute donc + pour s'assurer de convertir la concaténation en un nombre
+      };
+    });
+  }
+
+  return (
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      <Results input={userInput} />
+    </>
+  );
+}
+
+export default App;
